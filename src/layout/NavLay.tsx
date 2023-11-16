4090fb38e6c9739f58887d585b4fe8adf0be7f04
AgungDjalil <agungfirmansyahdjalil@gmail.com>
2023-11-16 11:20:33 +0700

    homepage

--- src/layout/NavLay.tsx
+++ src/layout/NavLay.tsx
@@ -2,9 +2,18 @@ import { Outlet } from "react-router-dom";
 
 export function NavLay() {
     return (
-        <>
-            <h1>ini navbar</h1>
+        <div className="bg-primaryBg h-screen">
+            <div className="flex justify-between mx-4">
+                <div className="mt-10">
+                    logo
+                </div>
+                <div className="flex font-bold mt-10">
+                    <button className="bg-white px-8 py-2 rounded-xl">Masuk</button>
+                    <button className="mx-16">Daftar</button>
+                </div>
+            </div>
+
             <Outlet />
-        </>
+        </div>
     )
 }
